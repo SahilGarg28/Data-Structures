25224b2f4ba8d806b112a347c9c260e42cad7f39
Sahil Garg <69622920+SahilGarg28@users.noreply.github.com>
2022-09-21 18:17:11 +0530

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+void swap(int &a, int &b)
+{
+    int temp=a;
+    a=b;
+    b=temp;
+}
+int main() {
+    // Write C++ code here
+    int arr[]={8,2,6,4,5,7};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            swap(arr[j],arr[j+1]);
+        }
+    }
+    cout<<"________________________________"<<endl;
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<endl;
+    
+
+    return 0;
+}
